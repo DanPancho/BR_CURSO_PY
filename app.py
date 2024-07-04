3686824a7f9a970f56af2edec98fe9f94e468555
DanPancho <daarcosa@puce.edu.ec>
2024-07-04 17:10:39 -0500

    [master]: connect BD

--- app.py
+++ app.py
@@ -1,42 +1,77 @@
 from flask import Flask, request, jsonify
+from flask_cors import cross_origin, CORS
 import jwt
+import mysql.connector
+from mysql.connector import Error
 import datetime
 
 app = Flask(__name__) #CREAMOS UNA INSTACIA DE NUESTRA APLICACION
 app.config['SECRET_KEY'] = 'SECRET_KEY'
+CORS(app)
+
+
+def connectDB():
+    try: 
+        connection = mysql.connector.connect(
+            host='127.0.0.1',
+            database='usuarios',
+            user='root',
+            password=''
+        )
+        if connection.is_connected():
+            return connection
+    except Error as e:
+        print("Error al conectar con la base de datos")
+        return None
+
+
 
 @app.route('/', methods=['GET'])
+@cross_origin(origins='*')  
 def hello():
     return jsonify({'respones': 'HOLA'})
 
 
 @app.route('/login', methods=['POST'])
+@cross_origin(origins='*')  
 def login():
     auth = request.json
 
     if auth['username'] == 'username' and auth['password'] == 'password':
         token =  jwt.encode(
-            {'user': auth['username'], 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=5)},
+            {'user': auth['username'], 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},
             app.config['SECRET_KEY'])
         return jsonify({'token': token}), 200
     else: 
         return jsonify({'message': 'Invalid credentials'}), 401
    
     
-   # return jsonify({'token', 'Credenciales no validas'}) , 401
-
-@app.route('/user', methods=['POST'])
-def user():
-    token = request.headers.get('x-acces-token')
-    if not token:
-        return jsonify({'message': 'No se ingreso toke'})
+def validaToken(token):
     try:
         data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
-        return jsonify({'message': 'TOKEN OK'})
+        return True
     except jwt.ExpiredSignatureError:
-        return jsonify({'response': 'Token expirado!'}), 401
+        return False
     except jwt.InvalidTokenError:
-        return jsonify({'response': 'Token is invalid!'}), 401
+        return False
+    
+
+
+@app.route('/users', methods=['GET'])
+@cross_origin(origins='*')  
+def users():
+    token = request.headers.get('x-acces-token')
+    if not token:
+        return jsonify({'message': 'No se ingreso token'})
+    elif validaToken(token):
+        conn = connectDB()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute('SELECT * FROM usuarios')
+        user = cursor.fetchall()
+        cursor.close()
+        return jsonify(user),200
+    return jsonify({'message': 'Error interno del servicio'}), 500
+   
 
 
 if __name__ == '__main__':
